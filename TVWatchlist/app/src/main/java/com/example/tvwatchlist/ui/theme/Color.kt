695b65ca7bb0a56c2b7263de970ceaf02c24b583
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2023-07-24 00:03:12 +0800

    Initial Commit

---
+++ TVWatchlist/app/src/main/java/com/example/tvwatchlist/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.tvwatchlist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DeepOrange = Color(0xFFFF5722)
+val LightOrange = Color(0xFFFF8A50)
+val DeepOrangeDark = Color(0xFFC41C00)
